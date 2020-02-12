920c73670c327fd6ed9ea62f64f9a964a93068e8
임경민 <ljlm0402@naver.com>
2020-02-12 11:16:48 +0900

    Add images

--- lib/default/src/server.ts
+++
@@ -1,9 +0,0 @@
-import createServer from './app';
-
-const PORT = process.env.PORT || 3000;
-const server = createServer();
-
-server.listen(+PORT, "0.0.0.0", (err, host) => {
-  if (err) throw err;
-  console.log(`server listening on ${host}`);
-});
